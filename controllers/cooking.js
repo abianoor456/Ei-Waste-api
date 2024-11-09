65f53901fa5831aa0e6422819bee55573d55230e
abianoor456 <abianoor789@gmail.com>
2024-11-10 01:24:51 +0200

    feat: add cooking get

--- controllers/cooking.js
+++ controllers/cooking.js
@@ -1,17 +1,27 @@
 const Cooking = require('../dal/models/cooking');
 const Product = require('../dal/models/products');
+const moment = require('moment');
 
 const get = async (req, res) => {
   try {
 
-    const data = await Cooking.find()
-     .populate({
-    path: 'batch_id',
-    populate: {
-      path: 'product_id',
-      model: 'Product'
-    }
-  });
+    const startOfMonth = moment('2023-11', 'YYYY-MM').startOf('month').toDate();
+    const endOfMonth = moment('2023-11', 'YYYY-MM').endOf('month').toDate();
+
+    // Query with the date range filter
+    const data = await Cooking.find({
+      $or: [
+        { batch_date_in: { $gte: startOfMonth, $lt: endOfMonth } },
+        { batch_date_out: { $gte: startOfMonth, $lt: endOfMonth } }
+      ]
+    })
+      .populate({
+        path: 'batch_id',
+        populate: {
+          path: 'product_id',
+          model: 'Product'
+        }
+      });
 
     res.json({
       message: 'Data loaded successfully',
